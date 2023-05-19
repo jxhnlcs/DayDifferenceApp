d33aa9cc565b50d6018752d2852fa9d55b94a491
John Lucas <joohnzay@gmail.com>
2023-05-19 17:09:20 -0300

    finish

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 const App = () => {
   const [date1, setDate1] = useState('');
   const [date2, setDate2] = useState('');
   const [difference, setDifference] = useState('');
+  const [error, setError] = useState('');
+
+  const formatDateString = (inputDate) => {
+    let formattedDate = inputDate;
+    if (formattedDate.length === 2 || formattedDate.length === 5) {
+      formattedDate += '/';
+    }
+    return formattedDate.slice(0, 10);
+  };
+
+  const handleDate1Change = (text) => {
+    setDate1(formatDateString(text));
+  };
+
+  const handleDate2Change = (text) => {
+    setDate2(formatDateString(text));
+  };
 
   const calculateDifference = () => {
+    if (!date1 || !date2) {
+      setError('Preencha os campos de data');
+      return;
+    }
+
+    const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!regex.test(date1) || !regex.test(date2)) {
+      setError('Use apenas números para a data');
+      return;
+    }
+
+    setError('');
+
     const [day1, month1, year1] = date1.split('/');
     const [day2, month2, year2] = date2.split('/');
 
@@ -20,22 +49,39 @@ const App = () => {
     setDifference(differenceInDays.toString());
   };
 
+  const clearFields = () => {
+    setDate1('');
+    setDate2('');
+    setDifference('');
+    setError('');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Calculadora de Diferença de Dias</Text>
       <TextInput
         style={styles.input}
         placeholder="Digite a primeira data (dd/mm/aaaa)"
+        placeholderTextColor="#ccc"
         value={date1}
-        onChangeText={(text) => setDate1(text)}
+        onChangeText={handleDate1Change}
       />
       <TextInput
         style={styles.input}
         placeholder="Digite a segunda data (dd/mm/aaaa)"
+        placeholderTextColor="#ccc"
         value={date2}
-        onChangeText={(text) => setDate2(text)}
+        onChangeText={handleDate2Change}
       />
-      <Button title="Calcular" onPress={calculateDifference} />
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.button} onPress={calculateDifference}>
+          <Text style={styles.buttonText}>Calcular</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]} onPress={clearFields}>
+          <Text style={styles.buttonText}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+      {error ? <Text style={styles.error}>{error}</Text> : null}
       <Text style={styles.result}>
         Diferença em dias: {difference}
       </Text>
@@ -46,26 +92,52 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
+    justifyContent: 'center',
     padding: 16,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 32,
   },
   input: {
     width: '100%',
-    height: 40,
-    borderColor: 'gray',
+    height: 50,
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
     borderWidth: 1,
-    marginBottom: 8,
-    paddingHorizontal: 8,
+    borderRadius: 8,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+    fontSize: 16,
+  },
+  buttons: {
+    flexDirection: 'row',
+    marginBottom: 16,
+  },
+  button: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#007bff',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 15,
+    marginLeft: 15,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  error: {
+    color: 'red',
+    marginBottom: 16,
   },
   result: {
-    marginTop: 16,
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: 'bold',
   },
 });
